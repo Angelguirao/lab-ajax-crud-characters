fd17ba49c1860b4d44816e92644927b6b66431b8
Angel Guirao <angeguirao92@gmail.com>
2023-07-27 17:50:54 +0200

    solved until fetch one

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,70 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  // Function to get all characters
+  async getFullList() {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters`);
+      console.log("All Characters:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching all characters:', error.message);
+      return null;
+    }
   }
 
-  getOneRegister () {
-
+  // Function to get a single character by ID
+  async getOneRegister(id) {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters/${id}`);
+      console.log("Character by ID:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching character by ID:', error.message);
+      return null;
+    }
   }
 
-  createOneRegister () {
-
+  // Function to create a new character
+  async createOneRegister(newCharacter) {
+    try {
+      const response = await axios.post(`${this.BASE_URL}/characters`,newCharacter);
+      console.log("Created Character:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error creating character:', error.message);
+      return null;
+    }
   }
 
-  updateOneRegister () {
-
+  // Function to update a character by ID
+  async updateOneRegister(id, updatedCharacter) {
+    try {
+      const response = await axios.put(`${this.BASE_URL}/characters/${id}`, updatedCharacter);
+      console.log("Updated Character:", response.data);
+      return response.data;
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        console.error("Character not found");
+      } else {
+        console.error("Error updating character:", error.response.data);
+      }
+    }
   }
 
-  deleteOneRegister () {
-
+ // Function to delete a character by ID
+  async deleteOneRegister(id) {
+    try {
+      const response = await axios.delete(`${this.BASE_URL}/characters/${id}`);
+      console.log(response.data);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        console.error("Character not found");
+      } else {
+        console.error("Error deleting character:", error.response.data);
+      }
+    }
   }
-}
+  }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,70 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    try {
+      const allCharacters = await charactersAPI.getFullList();
+      displayCharacters(allCharacters);
+      console.log("All Characters:", allCharacters);
+    } catch (error) {
+      console.error('Error fetching all characters:', error.message);
+    }
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    event.preventDefault();
+    const charIdInput = document.getElementById('character-id').value;
+    try {
+      const character = await charactersAPI.getOneRegister(charIdInput);
+      displayCharacter(character);
+      console.log("Character by ID:", character);
+    } catch (error) {
+      console.error('Error fetching character by ID:', error.message);
+    }
   });
 });
+
+function displayCharacters(characters) {
+  const charactersContainer = document.querySelector('.characters-container');
+  charactersContainer.innerHTML = '';
+
+  characters.forEach(character => {
+    const characterCard = createCharacterCard(character);
+    charactersContainer.appendChild(characterCard);
+  });
+}
+
+function displayCharacter(character) {
+  const characterContainer = document.querySelector('.characters-container');
+  characterContainer.innerHTML = '';
+
+  const characterCard = createCharacterCard(character);
+  characterContainer.appendChild(characterCard);
+}
+
+function createCharacterCard(character) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('character-card');
+
+  const idDiv = document.createElement('div');
+  idDiv.textContent = `ID: ${character.id}`;
+  cardDiv.appendChild(idDiv);
+
+  const nameDiv = document.createElement('div');
+  nameDiv.textContent = `Name: ${character.name}`;
+  cardDiv.appendChild(nameDiv);
+
+  const occupationDiv = document.createElement('div');
+  occupationDiv.textContent = `Occupation: ${character.occupation}`;
+  cardDiv.appendChild(occupationDiv);
+
+  const cartoonDiv = document.createElement('div');
+  cartoonDiv.textContent = `Is a Cartoon: ${character.cartoon ? 'True' : 'False'}`;
+  cardDiv.appendChild(cartoonDiv);
+
+  const weaponDiv = document.createElement('div');
+  weaponDiv.textContent = `Weapon: ${character.weapon}`;
+  cardDiv.appendChild(weaponDiv);
+
+  return cardDiv;
+}
